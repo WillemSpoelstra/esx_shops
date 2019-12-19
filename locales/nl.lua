37b11c5e6dd6dbfe7ed83a3de7c7f6be3614f92d
Willem Spoelstra <16556575+WillemSpoelstra@users.noreply.github.com>
2019-12-19 16:51:21 +0100

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+  ['shop'] = 'winkel',
+  ['shops'] = 'winkels',
+  ['press_menu'] = 'druk op ~INPUT_CONTEXT~ om ~y~store~s~ te gebruiken.',
+  ['shop_item'] = '$%s',
+  ['bought'] = 'je kocht ~y~%sx~s~ ~b~%s~s~ voor ~r~€%s~s~',
+  ['not_enough'] = 'je hebt niet ~r~genoeg~s~ geld, je ~y~mist~s~ ~r~€%s~s~!',
+  ['player_cannot_hold'] = 'je hebt ~r~niet~s~ genoeg ~y~vrije ruimte~s~ in je inventaris!',
+  ['shop_confirm'] = 'koop %sx %s voor €%s?',
+  ['no'] = 'nee',
+  ['yes'] = 'ja',
+}
